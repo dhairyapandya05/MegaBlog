a9b706ea00326e231f819a9e27f8a3288dae9260
dhairyapandya05 <dhairyapandya05@gamil.com>
2025-06-04 09:37:10 +0530

    Refactor authentication flow, integrate Redux for state management, and update environment configuration files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import "./App.css";
+import {useDispatch} from "react-redux";
+import authService from "../src/appwrite/auth";
+import {login, logout} from "./store/authStore";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>dvc</>;
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  return !loading ? (
+    <div className="flex-wrap content-between bg-gray-400 min-h-screen flex ">
+      <div className="w-full block">
+        <Header />
+        <main>// TODO: {/* OUTLET */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
